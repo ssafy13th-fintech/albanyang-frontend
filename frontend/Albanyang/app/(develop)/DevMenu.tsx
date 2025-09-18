2fd5283eb05d8369a07b1361dd25453daeb817ac
심양관 <cash3543@naver.com>
2025-09-18 09:09:42 +0900

    [feat] mainpages demo 생성 #13

--- frontend/Albanyang/app/(develop)/DevMenu.tsx
+++ frontend/Albanyang/app/(develop)/DevMenu.tsx
@@ -2,9 +2,11 @@ import { useRouter } from 'expo-router';
 import { Button, View, Text } from 'react-native';
 
 const routes = [
-  { name: '로그인', path: '/login/Login' },
+  { name: '로그인', path: '/Login' },
   { name: '프로필', path: '/profile' },
-  { name: '메인페이지', path : '/albaMainPage/AlbaMainPage'}
+  { name: '알바 데모 메인페이지', path : '/AlbaMainPage'},
+  { name: '사장 메인페이지', path : '/EmployerMainPage' },
+  { name: '알바 메인페이지', path : '/EmployeeMainPage' }
 ];
 
 export default function DevMenu() {
